f0285331a62f5a2b794cdb4e1136c764774523c1
Briana Camp <brianarcamp@gmail.com>
2015-05-20 13:36:43 -0700

    create got it button and functionality

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var flashApp = angular.module("flash", []);
 flashApp.controller("CardController", function ($scope) {
   $scope.cards = [
-    { front: "1",
-      back: "2"
+    { front: "1- front",
+      back: "2 -back"
     },
-    { front: "3",
-      back: "4"
+    { front: "3 - front",
+      back: "4 - back"
     },
-    { front: "5",
-      back: "6"
+    { front: "5 - front",
+      back: "6 - back"
     }
 
   ];
@@ -29,6 +29,12 @@ flashApp.controller("CardController", function ($scope) {
     var numCards = $scope.cards.length;
     $scope.current = ($scope.current + step + numCards) % numCards;
     $scope.front = !$scope.front;
+  };
+
+  $scope.gotIt = function() {
+    $scope.front = true;
+    $scope.cards.splice($scope.current, 1);
+    $scope.current = $scope.current % $scope.cards.length;
   };
 
 
