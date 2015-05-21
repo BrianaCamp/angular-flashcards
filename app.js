08e3c35014d7a436b15c94113beafe389c16f9ac
Briana Camp <brianarcamp@gmail.com>
2015-05-21 14:52:55 -0700

    add add-card feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var flashApp = angular.module("flash", []);
+
 flashApp.controller("DeckController", function ($scope){
   var allCards = [
     { front: "1- front",
@@ -13,19 +14,31 @@ flashApp.controller("DeckController", function ($scope){
 
   ];
 
-  $scope.copyDeck = function() {
-    return [].concat(allCards);
+  var studyDeck;
+
+  $scope.refreshStudyDeck = function() {
+    return studyDeck = [].concat(allCards);
   };
+
+  $scope.addCard = function(front, back){
+    var newCard = {front: front, back: back};
+    allCards.push(newCard);
+    studyDeck.push(newCard);
+  };
+
+
+
 });
 
 flashApp.controller("StudyController", function ($scope) {
 
 
   function init() {
-    $scope.cards = $scope.$parent.copyDeck();
+    $scope.cards = $scope.$parent.refreshStudyDeck();
     $scope.current = 0; //card to display
     $scope.front = true;
   }
+
 
   init();
 
@@ -52,10 +65,16 @@ flashApp.controller("StudyController", function ($scope) {
   $scope.gotIt = function() {
     $scope.front = true;
     $scope.cards.splice($scope.current, 1);
-    $scope.current = $scope.current % $scope.cards.length;
+    $scope.current = ($scope.current % $scope.cards.length) || 0;
   };
 
 
 });
 
-flashApp.controller("CardsController", function () {});
+flashApp.controller("CardsController", function ($scope) {
+  $scope.addCard = function() {
+    $scope.$parent.addCard($scope.front, $scope.back);
+  };
+
+
+});
